5aef7d5426c0946fb1cadf207d121b017315fce3
neytrino <neytrino070902@yandex.ru>
2019-02-23 20:57:43 +0300

    Added registration form, registration page and form validation

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, FileField, DateField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, Optional
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +8,18 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    channel_name = StringField('Название канала', validators=[DataRequired()])
+    avatar = FileField('Выберите аватар')
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    birthday = DateField('Дата рождения', format='%d/%m/%Y', validators=[Optional()])
+    interests = StringField('Интересы')
+    about_channel = TextAreaField('Описание канала')
+    meta_tags = StringField('Мета-теги')
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    repeat_password = PasswordField('Повторите пароль', validators=[EqualTo('password')])
+    agree = BooleanField('Я согласен(-на) с пользовательским соглашением')
+    submit = SubmitField('Зарегистрироваться')
--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
-from app import app
+from app import app, db
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
-from app.forms import LoginForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm
+from app.models import User, Interests, MetaTags
 from werkzeug.urls import url_parse
 
 
@@ -28,6 +28,51 @@ def login():
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/sign-in', methods=['GET', 'POST'])
+def sign_in():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    print('ОК')
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is not None:
+            flash('Пользователь с таким именем уже существует.')
+        else:
+            file = request.files['avatar'].read()
+            user = User(username=form.username.data, channel_name=form.channel_name.data,
+                        avatar=file, email=form.email.data, birthday=form.birthday.data,
+                        about_channel=form.about_channel.data)
+            user.set_password(form.password.data)
+            interest = form.interests.data.split(',')
+
+            # Проверка, есть ли данный интерес в базе данных
+            for i in range(len(interest)):
+                interest[i] = interest[i].lower().rstrip().lstrip()
+                if Interests.query.filter_by(text=interest[i]).first() is None:
+                    intt = Interests(text=interest[i])
+                    db.session.add(intt)
+                    db.session.commit()
+                user.interests.append(Interests.query.filter_by(text=interest[i]).first())
+            meta_tags = form.meta_tags.data.split(',')
+
+            # Проверка, есть ли данный мета-тег в базе данных
+            for i in range(len(meta_tags)):
+                meta_tags[i] = meta_tags[i].lower().rstrip().lstrip()
+                if MetaTags.query.filter_by(text=meta_tags[i]).first() is None:
+                    meta_tag = Interests(text=meta_tags[i])
+                    db.session.add(meta_tag)
+                    db.session.commit()
+                user.meta_tags.append(MetaTags.query.filter_by(text=interest[i]).first())
+
+            db.session.add(user)
+            db.session.commit()
+            user = User.query.filter_by(username=form.username.data).first()
+            login_user(user)
+            return redirect(url_for('index'))
+    return render_template('register.html', form=form, title='Регистрация')
 
 
 @app.route('/logout')
